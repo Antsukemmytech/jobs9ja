b8efa1f5ac3486f8e3499f6264c1f86f3c414623
Kenneth Chukwu <chukingroup@gmail.com>
2024-06-20 13:42:08 +0100

    updated

--- jobs9ja/company/models.py
+++ jobs9ja/company/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
-    name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
     description = models.TextField()
     website = models.URLField(max_length=200, blank=True)
     email = models.EmailField(max_length=254, blank=True)
